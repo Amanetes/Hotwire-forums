d2dc08750eaca19de89246c3ef7d755f1de09f74
Sergey <ser_8904@mail.ru>
2022-10-26 21:06:42 +0300

    Discussion List

---
+++ app/controllers/discussions_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class DiscussionsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @discussions = Discussion.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'discussions/index'
   devise_for :users
+
   root to: 'main#index'
+
+  resources :discussions, only: [:index]
 end
