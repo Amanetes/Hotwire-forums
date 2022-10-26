32be9193762694ca708ff5ba91f87b8e2f010e82
Sergey <ser_8904@mail.ru>
2022-10-26 15:28:00 +0300

    Flash Messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_class(type)
+    mapping = {
+      success: 'alert-success',
+      notice: 'alert-info',
+      alert: 'alert-danger',
+      error: 'alert-danger',
+      warn: 'alert-warning'
+    }
+    mapping[type.to_sym]
+  end
 end
