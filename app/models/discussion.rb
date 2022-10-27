9b5d9278ceb94f6f6b21e9907c89ea20ae6513d5
Sergey <ser_8904@mail.ru>
2022-10-27 20:35:27 +0300

    Realtime Discussions (for everyone!)

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,6 +4,12 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   validates :name, presence: true
 
+  # https://github.com/hotwired/turbo-rails/blob/main/app/models/concerns/turbo/broadcastable.rb
+
+  after_create_commit -> { broadcast_prepend_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
+
   # We can use to_param on our models to get a more friendly URL, without needing a gem like FriendlyId
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
