01b52911d8268c32828d295da2461e03e0a7cbc2
Sergey <ser_8904@mail.ru>
2022-10-27 19:59:55 +0300

    to_param for friendly links

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,4 +3,9 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   validates :name, presence: true
+
+  # We can use to_param on our models to get a more friendly URL, without needing a gem like FriendlyId
+  def to_param
+    "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
 end
